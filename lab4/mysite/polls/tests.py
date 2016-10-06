910f38bfb49c35e4199837649fc72d98eec248eb
r-hu1 <randyhu3089@gmail.com>
2016-10-06 15:45:40 -0600

    lab5

--- lab4/mysite/polls/tests.py
+++ lab4/mysite/polls/tests.py
@@ -1,3 +1,13 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question
+
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() +datetime.timedelta(days=3)
+        future_question = Question (pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
